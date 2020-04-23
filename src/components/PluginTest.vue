bbff552d1c12bc391b90c830fa37cad113e2c9d2
lvl@tiG423 <shuagxialol@163.com>
2020-04-23 10:41:24 +0800

    更新

--- src/components/PluginTest.vue
+++ src/components/PluginTest.vue
@@ -44,7 +44,6 @@ export default class PluginTest extends Vue {
       //     z: 7000000
       //   }
       // }
-<<<<<<< HEAD
     },
     bookmarks: [
       {
@@ -60,23 +59,6 @@ export default class PluginTest extends Vue {
         }
       }
     ]
-=======
-    }
-    // bookmarks: [
-    //   {
-    //     name: "china",
-    //     camera: {
-    //       heading: 0,
-    //       tilt: 9.15,
-    //       position: {
-    //         x: 105.508849,
-    //         y: 22.581284,
-    //         z: 7000000
-    //       }
-    //     }
-    //   }
-    // ]
->>>>>>> 8b4c2ea4bcfafc5dd8e8ddc464d568c3fbdb8028
   };
   private mapLoaded() {
 	console.log("map loaded");
@@ -127,7 +109,6 @@ export default class PluginTest extends Vue {
         size: 20000,
         anchor: "bottom"
       },
-<<<<<<< HEAD
       overlays: [{ id: "test001", geometry: { x: 121.418924, y: 31.157101 },fields:{name:"测试2",featureid:"0002"} },
       { id: "test002", geometry: { x: 121.318924, y: 31.157101 },fields:{name:"测试3",featureid:"0003"} },
       { id: "test003", geometry: { x: 121.418924, y: 31.257101 },fields:{name:"测试4",featureid:"0001"} }],
@@ -144,34 +125,6 @@ export default class PluginTest extends Vue {
     let map=(this.$refs.gisViewer as any)
     axios.get("config/point.json").then(res=>{
       map.addOverlaysCluster(res.data)
-=======
-      overlays: [
-        {
-          id: "test001",
-          geometry: { x: 121.418924, y: 31.157101 },
-          fields: { name: "测试2", featureid: "0002" }
-        },
-        {
-          id: "test002",
-          geometry: { x: 121.318924, y: 31.157101 },
-          fields: { name: "测试3", featureid: "0003" }
-        },
-        {
-          id: "test003",
-          geometry: { x: 121.418924, y: 31.257101 },
-          fields: { name: "测试4", featureid: "0001" }
-        }
-      ],
-      showPopup: true,
-      autoPopup: true,
-      defaultInfoTemplate: {
-        title: "1212",
-        content: "name:<br/><button>{name}</button>"
-      },
-      defaultButtons: [{ label: "确认报警", type: "confirmAlarm" }],
-      showToolTip: true,
-      toolTipContent: "{name}"
->>>>>>> 8b4c2ea4bcfafc5dd8e8ddc464d568c3fbdb8028
     });
   }
   private btn_test2()
