2394398d0a6a38dbd082f318bf64d5022bb49a9e
lvl@tiG423 <shuagxialol@163.com>
2020-05-09 15:47:24 +0800

    ..

--- src/components/PluginTest.vue
+++ src/components/PluginTest.vue
@@ -21,9 +21,9 @@ import axios from "axios";
 export default class PluginTest extends Vue {
   private mapConfig = {
     //arcgis_api: "http://localhost:8090/arcgis_js_api_4/",
-    //arcgis_api: "http://128.64.130.247:8219/baidumap/jsapi/api.js",
+    arcgis_api: "http://128.64.130.247:8219/baidumap/jsapi/api.js",
     //arcgis_api: "http://128.64.151.245:8019/baidumap/jsapi/api.js",
-    arcgis_api:"http://localhost:8090/baidu/BDAPI.js",
+    //arcgis_api:"http://localhost:8090/baidu/BDAPI.js",
     theme: "vec", //dark,vec
     baseLayers: [
       {
