31d2b1fc6c22bae411d6ac1a6e9856008a2d6254
lvl@tiG423 <shuagxialol@163.com>
2020-05-30 17:35:22 +0800

    测试删除高德ts库版

--- src/plugin/gis-viewer/MapAppGaode.ts
+++ src/plugin/gis-viewer/MapAppGaode.ts
@@ -1,4 +1,4 @@
-import { loadScript, ILoadScriptOptions } from "esri-loader";
+import {loadScript, ILoadScriptOptions} from 'esri-loader';
 import {
   IMapContainer,
   IOverlayParameter,
@@ -11,46 +11,75 @@ import {
   IFindParameter,
   IResult,
   IDistrictParameter
-} from "@/types/map";
-import { OverlayGaode } from "@/plugin/gis-viewer/widgets/OverlayGaode";
-import { JurisdictionPolice } from "./widgets/GD/JurisdictionPolice";
-import { HeatMap } from "./widgets/GD/HeatMap";
-import "@amap/amap-jsapi-types";
+} from '@/types/map';
+import {OverlayGaode} from '@/plugin/gis-viewer/widgets/OverlayGaode';
+import {JurisdictionPolice} from '@/plugin/gis-viewer/widgets/GD/JurisdictionPolice';
+import {HeatMap} from '@/plugin/gis-viewer/widgets/GD/HeatMap';
+
+declare let AMap: any;
 
 export default class MapAppGaode implements IMapContainer {
-  public view!: AMap.Map;
+  public view!: any;
   public baseLayers: Array<any> = [];
   public showGisDeviceInfo: any;
 
-  public async initialize(mapConfig: any, mapContainer: string) {
-    let apiUrl = mapConfig.arcgis_api || mapConfig.api_url;
+  public async initialize(mapConfig: any, mapContainer: string): Promise<void> {
+    let apiUrl = mapConfig.arcgis_api; //"http://localhost:8090/baidu/BDAPI.js";
     let plugins =
-      "&plugin=AMap.DistrictSearch,AMap.Heatmap,AMap.CustomLayer,AMap.ControlBar";
-    if(apiUrl.indexOf("v=2")>-1)
-    {
-      plugins =
-      "&plugin=AMap.DistrictSearch,AMap.HeatMap,AMap.CustomLayer,AMap.ControlBar";
-    }  
+      '&plugin=AMap.DistrictSearch,AMap.Heatmap,AMap.CustomLayer,AMap.ControlBar';
     apiUrl = apiUrl + plugins;
+    let view: any;
     await loadScript({
-      url: apiUrl
+      url: `${apiUrl}`
     });
-    this.view = new AMap.Map(mapContainer, mapConfig.options);
-    return new Promise(resole => {
-      this.view.on("complete", () => {
-        if (mapConfig.baseLayers) {
-          mapConfig.baseLayers.forEach((element: any) => {
-            this.createLayer(this.view, element);
-          });
-        }
-        resole();
+    const apiRoot = mapConfig.arcgis_api.substring(0, apiUrl.lastIndexOf('/'));
+    console.log(apiRoot);
+
+    view = new AMap.Map(mapContainer, mapConfig.options);
+
+    let gisUrl = mapConfig.gisServer;
+    if (mapConfig.baseLayers) {
+      mapConfig.baseLayers.forEach((element: any) => {
+        this.createLayer(view, element);
+      });
+    }
+    this.view = view;
+    this.view.gisServer = gisUrl;
+  }
+
+  private async loadOtherScripts(scriptUrls: string[]): Promise<any> {
+    let promises = scriptUrls.map((url) => {
+      return new Promise((resolve, reject) => {
+        const scriptElement = document.createElement('script');
+        scriptElement.src = url;
+        scriptElement.onload = resolve;
+        document.body.appendChild(scriptElement);
+      });
+    });
+    return new Promise((resolve) => {
+      Promise.all(promises).then((e) => {
+        resolve(e);
       });
     });
   }
-
+  //得到url中的ip和port
+  private getIpPort(url: string): string {
+    let urls = url.split('/');
+    let ip: string = '';
+    for (let el in urls) {
+      if (el.indexOf(':') > 0 || el.indexOf('.') > 0) {
+        ip = el;
+        break;
+      }
+    }
+    if (ip === '') {
+      ip = urls[2];
+    }
+    return ip;
+  }
   public createLayer(view: any, layer: any) {
     switch (layer.type) {
-      case "traffic":
+      case 'traffic':
         let trafficlayer = new AMap.TileLayer.Traffic({
           autoRefresh: true, //是否自动刷新，默认为false
           interval: 60 //刷新间隔，默认180s
@@ -59,8 +88,8 @@ export default class MapAppGaode implements IMapContainer {
           view.add(trafficlayer);
         }
         this.baseLayers.push({
-          label: layer.label || "",
-          type: layer.type || "",
+          label: layer.label || '',
+          type: layer.type || '',
           layer: trafficlayer,
           visible: layer.visible !== false
         });
@@ -72,12 +101,10 @@ export default class MapAppGaode implements IMapContainer {
     overlay.showGisDeviceInfo = this.showGisDeviceInfo;
     return await overlay.addOverlays(params);
   }
-
-  public async findFeature(params: IFindParameter): Promise<IResult> {
+  public async findFeature(params: IFindParameter) {
     const overlay = OverlayGaode.getInstance(this.view);
-    return await overlay.findFeature(params);
+    await overlay.findFeature(params);
   }
-
   public async addOverlaysCluster(params: IOverlayClusterParameter) {}
 
   public async addHeatMap(params: IHeatParameter) {
@@ -119,7 +146,7 @@ export default class MapAppGaode implements IMapContainer {
 
   public showLayer(params: ILayerConfig) {
     console.log(params);
-    this.baseLayers.forEach(baselayer => {
+    this.baseLayers.forEach((baselayer) => {
       if (
         (params.label && baselayer.label === params.label) ||
         (params.type && baselayer.type === params.type)
@@ -132,7 +159,7 @@ export default class MapAppGaode implements IMapContainer {
     });
   }
   public hideLayer(params: ILayerConfig) {
-    this.baseLayers.forEach(baselayer => {
+    this.baseLayers.forEach((baselayer) => {
       if (
         (params.label && baselayer.label === params.label) ||
         (params.type && baselayer.type === params.type)
